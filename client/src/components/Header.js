fe0f9d8d85b808ec43a3b7a43cff21a8d7eee19f
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-06-12 23:33:18 -0500

    Initial commit of header items

---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import background image";
+
+
+function Header() {
+    return (
+        <header style={headerStyle}>
+            <h1>(React) Google Books Search</h1>
+            <p>Search for and Save Books of Interest</p>
+        </header>
+    );
+}
+
+export default Header;
