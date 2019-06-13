be0114a52ba1b55e6b593a4d495e49a0e2ce5bb0
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-06-12 23:35:18 -0500

    Header updated paragraph text. Nav.js initial commit

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
     return (
         <header style={headerStyle}>
             <h1>(React) Google Books Search</h1>
-            <p>Search for and Save Books of Interest</p>
+            <p>Search for and Save Books that you would like to read.</p>
         </header>
     );
 }
