6d36c25380ccee70ffdc6516590e0e8562b6412a
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-06-13 00:10:51 -0500

    Added css styling to page and background image to header.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,19 @@
 import React from "react";
-// import background image";
+import heroImage from "../images/bookshelf.jpg";
+
+const headerStyle = {
+    "backgroundImage": `url(${heroImage})`,
+    "maxWidth": "100vw",
+    height: "calc(100vw * .43333)",
+    "backgroundSize": "cover",
+    position: "relative"
+  }
 
 
 function Header() {
     return (
         <header style={headerStyle}>
-            <h1>(React) Google Books Search</h1>
+            <h1>Google Books Search</h1>
             <p>Search for and Save Books that you would like to read.</p>
         </header>
     );
